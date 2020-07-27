e0fac5674ef68a309d53394731913b68010eef10
Lynne <lynne@Kotiss-MacBook-Pro.local>
2020-07-26 19:36:01 -0700

    updates book schema

--- models/book.js
+++ models/book.js
@@ -3,12 +3,23 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   genre: { type: String },
-  ISBN: { type: String, required: true },
+  ISBN: {
+    type: String,
+    required: true,
+    validate: {
+      validator: async function(value) {
+        const book = await this.constructor.findOne({ ISBN: value});
+        if (book) {
+          throw new Error(`ISBN: ${value} already in use.`);
+        }
+      }
+    }},
   authorId: { type: String, required: true },
   blurb: { type: String },
   publicationYear: { type: Number, required: true },
   pageCount: { type: Number, required: true }
 });
 
-
+bookSchema.index({ authorId: 1 })
+bookSchema.index({ ISBN: 1 })
 module.exports = mongoose.model("books", bookSchema);
